02c2644fada21de2007f1b3cbd5a1e716b84abb5
Pethanan <105306532+Pethanan@users.noreply.github.com>
2022-11-26 19:07:24 +0530

    DOM Manipulations - child, sibling, parents

--- app.js
+++ app.js
@@ -1,13 +1,58 @@
+const parentOfItems = document.querySelector("#items").parentElement;
 
-const nthChild = document.querySelector("li:nth-child(2)");
-nthChild.style.color = "red";
+//parentOfItems.textContent = "this is the parent of items ";
 
-const oddChild = document.querySelectorAll("li:nth-child(odd)");
-for(let i=0; i<oddChild.length; i++)
-  {
-  oddChild[i].style.backgroundColor = "green";
-  }
+const lastelementch = document.querySelector("#items").lastElementChild;
+
+lastelementch.style.color = "blue";
+
+const lastCh = document.querySelector("#items").lastChild;
+
+console.log(lastCh);
+
+const firstelementchi = document.querySelector("#items").firstElementChild;
+
+firstelementchi.style.backgroundColor ="orange";
+
+const firstchi = document.querySelector("#items").firstChild;
+
+firstchi.style.color = "red";
+
+const nextsibl = document.querySelector("#thirdItem").nextSibling;
+
+nextsibl.textContent= "this is the next sibling of third item";
+
+const nextelementsibl = document.querySelector("#thirdItem").nextElementSibling;
+
+nextelementsibl.textContent = "this is the next sibling element of third item";
+
+const prevsibl = document.querySelector("#thirdItem").previousSibling;
+
+prevsibl.textContent= "this is the prev sibling of third item";
+
+const previouselementsibl = document.querySelector("#thirdItem").previousElementSibling;
+
+previouselementsibl.textContent = "this is the next sibling element of third item";
 
 
 
+let newDiv = document.createElement("div");
+
+
+
+newDiv.setAttribute("title", "attributeSet");
+
+let newTextElement = document.createTextNode("newly added text node");
+
+newDiv.appendChild(newTextElement);
+
+
+
+let container = document.querySelector("header .container");
+
+let h1 = document.querySelector("header h1")
+
+
+
+container.insertBefore(newDiv, h1);
 
